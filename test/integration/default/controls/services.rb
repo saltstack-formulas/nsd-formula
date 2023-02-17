b177857046c1ef9d9a44778a472d0e512e397208
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2023-02-17 21:53:30 +0100

    style(syntax): resolve linting errors+warnings
    Resolve all rubocop, rst-lint, salt-lint and yamllint complaints.
    
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- test/integration/default/controls/services.rb
+++ test/integration/default/controls/services.rb
@@ -22,38 +22,38 @@ control 'nsd.service.running' do
   end
 
   describe command('nslookup -port=53530 ns1.example.test 127.0.0.1') do
-    its('stdout') { should match "192.168.0.1" }
+    its('stdout') { should match '192.168.0.1' }
   end
 
   describe command('nslookup -port=53530 ns2.example.test 127.0.0.1') do
-    its('stdout') { should match "192.168.0.2" }
+    its('stdout') { should match '192.168.0.2' }
   end
 
   describe command('nslookup -port=53530 example.test 127.0.0.1') do
-    its('stdout') { should match "192.168.0.10" }
+    its('stdout') { should match '192.168.0.10' }
   end
 
   describe command('nslookup -port=53530 www.example.test 127.0.0.1') do
-    its('stdout') { should match "192.168.0.10" }
+    its('stdout') { should match '192.168.0.10' }
   end
 
   describe command('nslookup -port=53530 mail.example.test 127.0.0.1') do
-    its('stdout') { should match "192.168.0.11" }
+    its('stdout') { should match '192.168.0.11' }
   end
 
   describe command('nslookup -port=53530 192.168.0.1 127.0.0.1') do
-    its('stdout') { should match "ns1.example.test" }
+    its('stdout') { should match 'ns1.example.test' }
   end
 
   describe command('nslookup -port=53530 192.168.0.2 127.0.0.1') do
-    its('stdout') { should match "ns2.example.test" }
+    its('stdout') { should match 'ns2.example.test' }
   end
 
   describe command('nslookup -port=53530 192.168.0.10 127.0.0.1') do
-    its('stdout') { should match "example.test" }
+    its('stdout') { should match 'example.test' }
   end
 
   describe command('nslookup -port=53530 192.168.0.11 127.0.0.1') do
-    its('stdout') { should match "mail.example.test" }
+    its('stdout') { should match 'mail.example.test' }
   end
 end
